44f4d69e28a1c34dd4dfd2ff8cc61b4d12435836
costia <p.costia@gmail.com>
2021-07-10 20:05:25 +0300

    multiple sources

--- main.pyw
+++ main.pyw
@@ -5,12 +5,14 @@ from redditImageSource import RedditImageSource
 import logging
 import threading
 import time 
+import random
 
 class ChangeWallpaperThread(threading.Thread):
     def __init__(self,app):
         super(ChangeWallpaperThread, self).__init__()
         self.app = app
         self.stopEvent = threading.Event()
+        self.log = logging.getLogger('WallpaperChanger')
     
     def stop(self):
         self.stopEvent.set()
@@ -19,7 +21,9 @@ class ChangeWallpaperThread(threading.Thread):
         while not self.stopEvent.is_set():
             image = None
             while not image:
-                image = self.app.imageSource.getImage()
+                selectedSource = self.app.imageSources[random.randint(0,len(self.app.imageSources)-1)]
+                self.log.info(f'ChangeWallpaperThread: selected source {selectedSource.getName()}')
+                image = selectedSource.getImage()
                 time.sleep(1)
                 if image:
                     setWallpaper(image)
@@ -29,19 +33,27 @@ class MainApp:
     def __init__(self):
         log = logging.getLogger('WallpaperChanger')
         log.setLevel(logging.INFO)
-        fileHandler = logging.FileHandler('log.txt')
+        fileHandler = logging.FileHandler('log.txt',encoding='utf-8')
         fileHandler.setLevel(logging.INFO)
         formatter = logging.Formatter('%(asctime)s, %(levelname)s: %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
         fileHandler.setFormatter(formatter)
         log.addHandler(fileHandler)
         log.info(f'MainApp: started')
 
+        
+        self.imageSources  =[]
         redditArgs = {
             'width':1920,
             'height':1080,
             'subreddit':'wallpaper'
         }
-        self.imageSource  = RedditImageSource(redditArgs)
+        self.imageSources.append(RedditImageSource(redditArgs))
+        redditArgs = {
+            'width':1920,
+            'height':1080,
+            'subreddit':'wallpapers'
+        }
+        self.imageSources.append(RedditImageSource(redditArgs))
 
         self.wallpaperReplaceThread = ChangeWallpaperThread(self)
         self.wallpaperReplaceThread.start()
--- redditImageSource.py
+++ redditImageSource.py
@@ -4,6 +4,7 @@ import base64
 import json
 import random
 import os
+from urllib.parse import urlparse
 
 from wx.core import INT64_MAX
 
@@ -21,37 +22,57 @@ class RedditImageSource:
             os.mkdir(self.tempDir)
 
         self.tokenEndpoint = 'https://www.reddit.com/api/v1/access_token'
-        self.APIendpoint = f'https://oauth.reddit.com/r/{self.subreddit}/random.json?limit=1'
+        self.APIendpoint = f'https://oauth.reddit.com/r/{self.subreddit}/'
+        self.APIendpointRandom = self.APIendpoint+'random.json'
+        self.APIendpointSearch = self.APIendpoint+'search.json'
         self.headerData = 'QmFzaWMgT1ROS04yWnNiRGhJTTB0c1NXWk9lVVJoYUhadWR6bz0='
         self.userAgent = str(random.randint(0,INT64_MAX))
     
+    def getName(self):
+        return self.subreddit
+
     def getImage(self):
         headers={'Authorization':base64.b64decode(self.headerData).decode(),'User-Agent':self.userAgent}
         params = {'grant_type':'https://oauth.reddit.com/grants/installed_client','device_id':'DO_NOT_TRACK_THIS_DEVICE'}
         response = requests.post(self.tokenEndpoint,headers=headers,params=params)
         response = json.loads(response.content)
         if 'access_token' not in response:
-            self.log.error(f'RedditImageSource {self.subreddit} failed to get access token')
+            self.log.error(f'RedditImageSource: {self.subreddit} failed to get access token')
             return None
         accessToken=response['access_token']
 
         headers={'Authorization':f'Bearer {accessToken}','User-Agent':self.userAgent}
-        response = requests.get(self.APIendpoint,headers=headers)
-        response = json.loads(response.content)
-        postData = response[0]['data']['children'][0]['data']
-        permalink = postData['permalink']
+        params = {'limit':1}
+        response = requests.get(self.APIendpointRandom,headers=headers, params=params)
+        parsedUrl = urlparse(response.url)
+        if not parsedUrl.path.startswith(f'/r/{self.subreddit}/comments/'):
+            self.log.warning(f'RedditImageSource: {self.subreddit} does not support random fetch')
+            sortTypes = ['relevance','hot','top','new','comments']
+            sortType = sortTypes[random.randint(0,len(sortTypes)-1)]
+            timeLimits = ['hour','day','week','month','year','all']
+            timeLimit = timeLimits[random.randint(0,len(timeLimits)-1)]
+            params = {'limit':100,'restrict_sr':1,'sort':sortType,'t':timeLimit}
+            response = requests.get(self.APIendpoint,headers=headers,params=params)
+            response = json.loads(response.content)
+            response = response['data']['children']
+            postData = response[random.randint(0,len(response)-1)]['data']
+            pass
+        else:
+            response = json.loads(response.content)
+            postData = response[0]['data']['children'][0]['data']
+        permalink = 'www.reddit.com'+postData['permalink']
         if not 'post_hint' in postData or postData['post_hint']!='image':
-            self.log.error(f'RedditImageSource {self.subreddit} unsupported post type {permalink}')
+            self.log.error(f'RedditImageSource: {self.subreddit} unsupported post type {permalink}')
             return None
         
         imageData = postData['preview']['images'][0]['source']
         currentAR = imageData['width']/imageData['height']
         currentMP = imageData['width']*imageData['height']
         if abs(currentAR-self.requiredAR)/self.requiredAR>0.1:
-            self.log.error(f'RedditImageSource {self.subreddit} incompatible aspect ratio')
+            self.log.error(f'RedditImageSource: {self.subreddit} incompatible aspect ratio {permalink}')
             return None
         if currentMP<self.minMP:
-            self.log.error(f'RedditImageSource {self.subreddit} small image')
+            self.log.error(f'RedditImageSource: {self.subreddit} small image {permalink}')
             return None
         
         response = requests.get(postData['url'])
@@ -61,4 +82,6 @@ class RedditImageSource:
         for data in response:
             f.write(data)
         f.close()
+
+        self.log.info(f'RedditImageSource: {self.subreddit} fetched {permalink}')
         return outFile
