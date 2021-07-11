e9e4ac31c9ac2ad69e57c45a90eea801d9639b55
costia <p.costia@gmail.com>
2021-07-11 18:07:33 +0300

    History dummy GUI tab

--- GUI/wallpaperFrame.py
+++ GUI/wallpaperFrame.py
@@ -7,17 +7,21 @@ from ImageSources import getSourceTypes
 
 class WallpaperFrame(wx.Frame):
     def __init__(self,wxApp,config):
-        windowWidth = 300
-        startX = 5
-
         super(WallpaperFrame, self).__init__(None, style= wx.CAPTION |	 wx.CLOSE_BOX | wx.MINIMIZE_BOX)
         self.SetTitle(Resources['APP_NAME'])
         self.SetIcon(wx.Icon(wx.Bitmap(Resources['ICON_PATH'])))
         
+        self.windowWidth = 300
+        self.windowHeight = 490+30
+        self.panelHeight = 490
+        self.timeSelections = {'5min':5,'10min':10,'30min':30,'1h':60,'4h':60*4,'12h':60*12,'24h':60*24}
+
         self.sourceTypes = getSourceTypes()
         self.config = config
         self.wxApp = wxApp
         self.log = logging.getLogger('WallpaperChanger')
+
+        
 
         self.popupmenu = wx.Menu()
         createMenuItem(self.popupmenu, 'Minimize',  lambda x: self.wxApp.toggleShow())
@@ -27,52 +31,92 @@ class WallpaperFrame(wx.Frame):
         self.Bind(wx.EVT_CLOSE, lambda x: self.wxApp.handleExit())
         self.Bind(wx.EVT_ICONIZE, lambda x: self.wxApp.toggleShow())
 
-        self.timeSelections = {'5min':5,'10min':10,'30min':30,'1h':60,'4h':60*4,'12h':60*12,'24h':60*24}
+        self.selectMainPanelBtn = wx.Button(self,-1,'Main',pos=(0,0),size=(self.windowWidth/2+5,30))
+        self.selectHistoryPanelBtn = wx.Button(self,-1,'History',pos=(self.windowWidth/2+5,0),size=(self.windowWidth/2+10,30))
         
-        nextY = 10
-        
-        self.labelRefresh = wx.StaticText(self,label = "Wallpaper refresh delay:" ,pos=(startX,nextY),style = wx.ALIGN_LEFT) 
-        nextY += 20
+        self._buildMainPanel(40)
+        self._buildHistoryPanel(40)
 
-        self.timeSelect = wx.ComboBox(self,pos=(startX,nextY),size=(windowWidth,30),style=wx.CB_DROPDOWN|wx.CB_READONLY,choices=[x for x in self.timeSelections])
-        self.timeSelect.SetSelection(self._getCurrentTimeSelectID())
-        self.timeSelect.Bind(wx.EVT_COMBOBOX, self._onTimeSelectChange)
-        nextY += 30
-        self.changeNowButton = wx.Button(self,-1,'Change now',pos=(startX,nextY),size=(windowWidth,20))
         self.changeNowButton.Bind(wx.EVT_BUTTON, lambda x: self.wxApp.changeWallpaper())
+
+        self.selectMainPanelBtn.Bind(wx.EVT_BUTTON, self._showMainPanel)
+        self.selectHistoryPanelBtn.Bind(wx.EVT_BUTTON, self._showHistoryPanel)
+
+        self.timeSelect.Bind(wx.EVT_COMBOBOX, self._onTimeSelectChange)
+        self.minimizeButton.Bind(wx.EVT_BUTTON, lambda x: self.wxApp.toggleShow())
+        self.sourceAddButton.Bind(wx.EVT_BUTTON, self._addSource)
+        self.sourceConfig.Bind(wx.EVT_TEXT_ENTER,self._addSource)
+        self.sourceRemovButton.Bind(wx.EVT_BUTTON,self._removeSource)
+
+        self.baseFont = self.selectMainPanelBtn.GetFont()
+        self.selectMainPanelBtn.SetFont(self.selectMainPanelBtn.GetFont().Bold())
+        self.SetSize(size = (self.windowWidth+25,self.windowHeight))
+
+    def _showHistoryPanel(self,event):
+        self.historyPanel.Show(True)
+        self.mainPanel.Show(False)
+        self.selectMainPanelBtn.SetFont(self.baseFont)
+        self.selectHistoryPanelBtn.SetFont(self.baseFont.Bold())
+    
+    def _showMainPanel(self,event):
+        self.historyPanel.Show(False)
+        self.mainPanel.Show(True)
+        self.selectMainPanelBtn.SetFont(self.baseFont.Bold())
+        self.selectHistoryPanelBtn.SetFont(self.baseFont)
+    
+
+    def _buildHistoryPanel(self,nextY):
+        startX = 5
+        self.historyPanel = wx.Panel(self)
+        self.historyPanel.SetSize(size = (self.windowWidth+25,self.panelHeight))
+        self.historyPanel.SetPosition((0,0))
+
+        self.historListbox = wx.ListBox(self.historyPanel,pos=(startX,nextY),size=(self.windowWidth,self.windowHeight-40),style=wx.CB_READONLY)
+
+
+        self.historyPanel.Show(False)
+
+    def _buildMainPanel(self,nextY):
+        startX = 5
+        self.mainPanel = wx.Panel(self)
+        self.mainPanel.SetSize(size = (self.windowWidth+25,self.panelHeight))
+        self.mainPanel.SetPosition((0,0))
+
+        self.labelRefresh = wx.StaticText(self.mainPanel,label = "Wallpaper refresh delay:" ,pos=(startX,nextY),style = wx.ALIGN_LEFT) 
+        nextY += 20
+
+        self.timeSelect = wx.ComboBox(self.mainPanel,pos=(startX,nextY),size=(self.windowWidth,30),style=wx.CB_DROPDOWN|wx.CB_READONLY,choices=[x for x in self.timeSelections])
+        self.timeSelect.SetSelection(self._getCurrentTimeSelectID())
+        nextY += 30
+        self.changeNowButton = wx.Button(self.mainPanel,-1,'Change now',pos=(startX,nextY),size=(self.windowWidth,20))
         nextY += 30
 
         nextY += 20
-        self.labelSources = wx.StaticText(self,label = "Wallpaper sources:" ,pos=(startX,nextY),style = wx.ALIGN_LEFT) 
+        self.labelSources = wx.StaticText(self.mainPanel,label = "Wallpaper sources:" ,pos=(startX,nextY),style = wx.ALIGN_LEFT) 
         nextY += 20
-        self.sourceTypeSelect= wx.ComboBox(self,pos=(startX,nextY),size=(windowWidth,30),style=wx.CB_DROPDOWN|wx.CB_READONLY,choices=self.sourceTypes)
+        self.sourceTypeSelect= wx.ComboBox(self.mainPanel,pos=(startX,nextY),size=(self.windowWidth,30),style=wx.CB_DROPDOWN|wx.CB_READONLY,choices=self.sourceTypes)
         self.sourceTypeSelect.SetSelection(0)
         nextY += 25
-        self.sourceConfig = wx.TextCtrl(self,pos=(startX,nextY),size=(windowWidth,20),style = wx.TE_PROCESS_ENTER)
+        self.sourceConfig = wx.TextCtrl(self.mainPanel,pos=(startX,nextY),size=(self.windowWidth,20),style = wx.TE_PROCESS_ENTER)
         nextY += 20
-        self.sourceAddButton = wx.Button(self,-1,'Add new source',pos=(startX,nextY),size=(windowWidth,20))
+        self.sourceAddButton = wx.Button(self.mainPanel,-1,'Add new source',pos=(startX,nextY),size=(self.windowWidth,20))
         nextY += 30
 
-        self.sourcesListbox = wx.ListBox(self,pos=(startX,nextY),size=(windowWidth,100),style=wx.CB_DROPDOWN|wx.CB_READONLY)
+        self.sourcesListbox = wx.ListBox(self.mainPanel,pos=(startX,nextY),size=(self.windowWidth,100),style=wx.CB_DROPDOWN|wx.CB_READONLY)
         self._updateSourcesList()
         nextY += 100
-        self.sourceRemovButton = wx.Button(self,-1,'Remove source',pos=(startX,nextY),size=(windowWidth,20))
+        self.sourceRemovButton = wx.Button(self.mainPanel,-1,'Remove source',pos=(startX,nextY),size=(self.windowWidth,20))
         nextY += 30
 
         nextY += 20
-        self.minimizeButton = wx.Button(self,-1,'Minimize to tray',pos=(startX,nextY),size=(windowWidth,30))
-        self.minimizeButton.Bind(wx.EVT_BUTTON, lambda x: self.wxApp.toggleShow())
+        self.minimizeButton = wx.Button(self.mainPanel,-1,'Minimize to tray',pos=(startX,nextY),size=(self.windowWidth,30))
         nextY += 50
 
-        self.labelStatus = wx.TextCtrl(self,pos=(startX,nextY),size=(windowWidth,40),style = wx.TE_READONLY| wx.TE_MULTILINE| wx.TE_NO_VSCROLL ) 
+        self.labelStatus = wx.TextCtrl(self.mainPanel,pos=(startX,nextY),size=(self.windowWidth,40),style = wx.TE_READONLY| wx.TE_MULTILINE| wx.TE_NO_VSCROLL ) 
         self.labelStatus.SetLabelText('Status')
         self.labelStatus.SetBackgroundColour((173,173,173))
         nextY += 30
 
-        self.sourceAddButton.Bind(wx.EVT_BUTTON, self._addSource)
-        self.sourceConfig.Bind(wx.EVT_TEXT_ENTER,self._addSource)
-        self.sourceRemovButton.Bind(wx.EVT_BUTTON,self._removeSource)
-        self.SetSize(size = (windowWidth+25,nextY+50))
     
     def _updateSourcesList(self):
         sourcesListStrings = [ x['type']+':'+str(x['config']) for x in self.config['sources']]
