3aef5b8da5e612e169b06b3a74f649c5f4ba5813
costia <p.costia@gmail.com>
2021-07-13 14:10:12 +0300

    earthview: move first ID generation to getImage

--- src/ImageSources/earthviewImageSource.py
+++ src/ImageSources/earthviewImageSource.py
@@ -3,17 +3,15 @@ import logging
 from bs4 import BeautifulSoup
 import json
 import os
+import random
 
 from resources import Resources
 
 class EarthviewImageSource:
     def __init__(self,argsDict):
         self.log = logging.getLogger('WallpaperChanger')
-        response = requests.get('https://earthview.withgoogle.com/')
-        htmldata = BeautifulSoup(response.content.decode(),features="html.parser")
-        firstID = json.loads(htmldata.find('body')['data-photo'])
-        self.nextId = firstID['nextSlug']
         self.tempDir = Resources['TEMP_DIR']
+        self.nextId = None
 
     def getName(self):
         return 'earthview'
@@ -23,6 +21,12 @@ class EarthviewImageSource:
         return 'earthview'
 
     def getImage(self):
+        if not self.nextId or random.randint(0,9)==0:
+            response = requests.get('https://earthview.withgoogle.com/')
+            htmldata = BeautifulSoup(response.content.decode(),features="html.parser")
+            firstID = json.loads(htmldata.find('body')['data-photo'])
+            self.nextId = firstID['nextSlug']
+
         response = requests.get(f'https://earthview.withgoogle.com/_api/{self.nextId}.json')
         postData = json.loads(response.content)
         self.nextId = postData['nextSlug']
